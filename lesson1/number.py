64bb6d8e5d9d1f2156006046f364e1c2ebc71897
chypsik83 <shalyapina-dina@mail.ru>
2024-03-20 13:21:50 +0300

    Поправлен файл number

--- lesson1/number.py
+++ lesson1/number.py
@@ -12,19 +12,20 @@
 `.substract(<значение>)` вычитает указанное число из value
 """
 
+
 class Number:
 
     def __init__(self, value):
-        ...
+        self.value = value
 
-    def ...(self):
-        ...
+    def get(self):
+        return self.value
 
-    def ...:
-        ...
+    def add(self, num):
+        self.value += num
 
-    def ...:
-        ...
+    def substract(self, num):
+        self.value -= num
 
 
 # код для проверки 
